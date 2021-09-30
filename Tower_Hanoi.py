94637e58f9bf03eb911e12a5f8dcd2fb4f16c30c
JoseMiranda86 <josemiranda_86@hotmail.com>
2021-09-30 15:07:47 +0900

    Working on logic for new positions

--- Tower_Hanoi.py
+++ Tower_Hanoi.py
@@ -1,6 +1,6 @@
 print("\n\n\n\nWelcome to the game of the Tower of Hanoi! \n\n\n")
 
-Counter_roundes = 0
+Counter_rounds = 0
 
 # Defining the inial state of the grid
 data = {'A1':"s", 'A2':"000", 'A3':"00000",
@@ -19,3 +19,84 @@ while True:
     Line3 = [data['A3'], data['B3'], data['C3']]
     Line4 = [data['A4'], data['B4'], data['C4']]
     Line5 = [data['A5'], data['B5'], data['C5']]
+
+ # Def of function that will define the position of the elements
+
+    def position(n):
+        count = 0
+        for element in n:
+            if count == 0:
+                if len(element) == 9:
+                    New = "       " + element
+                    Newest_State.append(New)
+                    A1s = len(element)
+                    count = 1
+                elif len(element) == 7:
+                    New = "        " + element
+                    Newest_State.append(New)
+                    A1s = len(element)
+                    count = 1
+                elif len(element) == 5:
+                    New = "         " + element
+                    Newest_State.append(New)
+                    A1s = len(element)
+                    count = 1
+                elif len(element) == 3:
+                    New = "          " + element
+                    Newest_State.append(New)
+                    A1s = len(element)
+                    count = 1
+                else:
+                    New = "           " + element
+                    Newest_State.append(New)
+                    A1s = len(element)
+                    count = 1
+
+            elif count == 1:
+                if len(element) == 9:
+                    New = int(13 - ((A1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    B1s = len(element)
+                    count = 2
+                elif len(element) == 7:
+                    New = int(14 - ((A1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    B1s = len(element)
+                    count = 2
+                elif len(element) == 5:
+                    New = int(15 - ((A1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    B1s = len(element)
+                    count = 2
+                elif len(element) == 3:
+                    New = int(16 - ((A1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    B1s = len(element)
+                    count = 2
+                else:
+                    New = int(17 - ((A1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    B1s = len(element)
+                    count = 2
+            else:
+                if len(element) == 9:
+                    New = int(13 - ((B1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    C1s = len(element)
+                elif len(element) == 7:
+                    New = int(14 - ((B1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    C1s = len(element)
+                elif len(element) == 5:
+                    New = int(15 - ((B1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    C1s = len(element)
+                elif len(element) == 3:
+                    New = int(16 - ((B1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    C1s = len(element)
+                else:
+                    New = int(17 - ((B1s-1)/2))* " " + element
+                    Newest_State.append(New)
+                    C1s = len(element)  
+    
